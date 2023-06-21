cf72cd10e7170ca9a95efb4d2f2a514ff8f08b5b
Shivani Karikar <karikarshivani@gmail.com>
2023-06-21 14:26:37 -0400

    Fixed syntax errors by adding missing ends for ERB
    Signed-off-by: Shivani Karikar <karikarshivani@gmail.com>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -3,16 +3,7 @@ overlay_controls = input('overlay_controls')
 system_categorization = input('system_categorization')
 
 include_controls 'redhat-enterprise-linux-7-stig-baseline' do
-  unless overlay_controls.empty?
-    overlay_controls.each do |overlay_control|
-      control overlay_control do
-        impact 0.0
-        desc "caveat", "Not applicable for this CMS ARS 5.0 overlay, since the requirement is not included in CMS ARS 5.0"
-      end
-    end
-  end
 
-  
   ## NIST tags updated due to changes between NIST SP 800-53 rev 4 and rev 5 (https://csrc.nist.gov/csrc/media/Publications/sp/800-53/rev-5/final/documents/sp800-53r4-to-r5-comparison-workbook.xlsx)
 
   ## IA-2(6) incorporates withdrawn control IA-2(11)
@@ -53,5 +44,14 @@ include_controls 'redhat-enterprise-linux-7-stig-baseline' do
   ## SC-45 (1) incorporates withdrawn control AU-8 (1)
   control 'SV-204603' do
     tag nist: ["SC-45 (1)"]
+  end
+
+  unless overlay_controls.empty?
+    overlay_controls.each do |overlay_control|
+      control overlay_control do
+        impact 0.0
+        desc "caveat", "Not applicable for this CMS ARS 5.0 overlay, since the requirement is not included in CMS ARS 5.0"
+      end
+    end
   end
 end
